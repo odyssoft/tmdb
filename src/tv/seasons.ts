761ead55f567157c8e27599908ebc954d365d99a
SeannHewitson <seannkiely@icloud.com>
2022-09-03 11:43:10 +0100

    TMBD-4: feat - Started working on types

--- src/tv/seasons.ts
+++ src/tv/seasons.ts
@@ -1,14 +1,17 @@
 import Request from '../request'
 import * as Types from '../types'
 
-export const TVSeason = (apiKey: string, showId: number) => ({
+export const TVSeason = (apiKey: string, tv_id: number) => ({
   /**
    * Get the TV season details by id.
    * @param {number} season
    * @param {object} options
    * @returns Promise<TVSeasonDetails>
    */
-  GetDetails: (season: number, options?: Types.AppendOptions<Types.SeasonsAppend>) => {},
+  GetDetails: (
+    season: number,
+    options?: Types.AppendOptions<Types.SeasonsAppend>
+  ): Promise<Types.TVSeasonDetails> => Request(apiKey, `tv/${tv_id}/season/${season}`, options),
 
   /**
    * Get the aggregate credits for TV season.
@@ -17,7 +20,11 @@ export const TVSeason = (apiKey: string, showId: number) => ({
    * @param {options} options
    * @returns Promise<TVSeasonAggregateCredits>
    */
-  GetAggregateCredits: (season: number, options?: Types.Options) => {},
+  GetAggregateCredits: (
+    season: number,
+    options?: Types.Options
+  ): Promise<Types.TVSeasonAggregateCredits> =>
+    Request(apiKey, `tv/${tv_id}/season/${season}/aggregate_credits`, options),
 
   /**
    * Get the credits for TV season.
@@ -25,7 +32,8 @@ export const TVSeason = (apiKey: string, showId: number) => ({
    * @param {options} options
    * @returns Promise<TVSeasonCredits>
    */
-  GetCredits: (season: number, options?: Types.Options) => {},
+  GetCredits: (season: number, options?: Types.Options): Promise<Types.TVSeasonCredits> =>
+    Request(apiKey, `tv/${tv_id}/season/${season}/credits`, options),
 
   /**
    * Get the external ids for a TV season. We currently support the following external sources.
@@ -33,7 +41,8 @@ export const TVSeason = (apiKey: string, showId: number) => ({
    * @param {options} options
    * @returns Promise<TVExternalIds>
    */
-  GetExternalIds: (season: number, options?: Types.Options) => {},
+  GetExternalIds: (season: number, options?: Types.Options): Promise<Types.TVSeasonExternalIds> =>
+    Request(apiKey, `tv/${tv_id}/season/${season}/external_ids`, options),
 
   /**
    * Get the images that belong to a TV season.
@@ -41,7 +50,8 @@ export const TVSeason = (apiKey: string, showId: number) => ({
    * @param {options} options
    * @returns Promise<TVSeasonImages>
    */
-  GetImages: (season: number, options?: Types.Options) => {},
+  GetImages: (season: number, options?: Types.Options): Promise<Types.TVSeasonImages> =>
+    Request(apiKey, `tv/${tv_id}/season/${season}/images`, options),
 
   /**
    *
@@ -49,7 +59,8 @@ export const TVSeason = (apiKey: string, showId: number) => ({
    * @param {options} options
    * @returns Promise<TVSeasonTranslations>
    */
-  GetTranslations: (season: number, options?: Types.Options) => {},
+  GetTranslations: (season: number, options?: Types.Options): Promise<Types.TVSeasonTranslations> =>
+    Request(apiKey, `tv/${tv_id}/season/${season}/translations`, options),
 
   /**
    * Get the videos that have been added to a TV season.
@@ -57,7 +68,8 @@ export const TVSeason = (apiKey: string, showId: number) => ({
    * @param {options} options
    * @returns Promise<TVSeasonVideos>
    */
-  GetVideos: (season: number, options?: Types.Options) => {},
+  GetVideos: (season: number, options?: Types.Options): Promise<Types.TVSeasonVideos> =>
+    Request(apiKey, `tv/${tv_id}/season/${season}/videos`, options),
 })
 
 export default TVSeason
--- src/types/tv/seasons.ts
+++ src/types/tv/seasons.ts
@@ -1,1 +1,37 @@
-export interface TVSeasonDetails {}
+export interface TVSeasonDetails {
+  _id: string
+  air_date: string | null | undefined
+  episodes: TVSeasonEpisode[]
+  name: string
+  overview: string
+  id: number
+  poster_path: string | null | undefined
+  season_number: number
+}
+
+export interface TVSeasonEpisode {
+  air_date: string
+  episode_number: number
+  crew: Crew[]
+  guest_stars: GuestStar[]
+  id: number
+  name: string
+  overview: string
+  production_code: string
+  season_number: number
+  still_path: string
+  vote_average: number
+  vote_count: number
+}
+
+export interface TVSeasonAggregateCredits {}
+
+export interface TVSeasonCredits {}
+
+export interface TVSeasonExternalIds {}
+
+export interface TVSeasonImages {}
+
+export interface TVSeasonTranslations {}
+
+export interface TVSeasonVideos {}
