8ef20de24a757147eed033c36bcf205bfec199d1
seannhewitson <seannhewitson@icloud.com>
2025-09-26 20:24:42 +0100

    Version bump

--- src/types/discover.ts
+++ src/types/discover.ts
@@ -15,78 +15,78 @@ export type DiscoverSort =
   | 'vote_count.desc'
 
 export type DiscoverOptions = {
-  'air_date.gte': string
-  'air_date.lte': string
-  first_air_date_year: number
-  'first_air_date.gte': string
-  'first_air_date.lte': string
+  'air_date.gte'?: string
+  'air_date.lte'?: string
+  first_air_date_year?: number
+  'first_air_date.gte'?: string
+  'first_air_date.lte'?: string
   /**
    * defaults to false
    */
-  include_adult: boolean
+  include_adult?: boolean
   /**
    * defaults to false
    */
-  include_null_first_air_dates: boolean
+  include_null_first_air_dates?: boolean
   /**
    * Defaults to en-US
    */
-  language: string
+  language?: string
   /**
    * Defaults to 1
    */
-  page: number
-  screened_theatrically: boolean
+  page?: number
+  screened_theatrically?: boolean
   /**
    * Defaults to popularity.desc
    */
-  sort_by: DiscoverSort
-  timezone: string
-  'vote_average.gte': number
-  'vote_average.lte': number
-  'vote_count.gte': number
-  'vote_count.lte': number
+  sort_by?: DiscoverSort
+  timezone?: string
+  'vote_average.gte'?: number
+  'vote_average.lte'?: number
+  'vote_count.gte'?: number
+  'vote_count.lte'?: number
   /**
    * use in conjunction with with_watch_monetization_types or with_watch_providers
    */
-  watch_region: string
+  watch_region?: string
   /**
    * can be a comma (AND) or pipe (OR) separated query
    */
-  with_companies: string
+  with_companies?: string
   /**
    * can be a comma (AND) or pipe (OR) separated query
    */
-  with_genres: string
+  with_genres?: string
   /**
    * can be a comma (AND) or pipe (OR) separated query
    */
-  with_keywords: string
-  with_networks: number
-  with_origin_country: string
-  with_original_language: string
-  'with_runtime.gte': number
-  'with_runtime.lte': number
+  with_keywords?: string
+  with_networks?: number
+  with_origin_country?: string
+  with_original_language?: string
+  'with_runtime.gte'?: number
+  'with_runtime.lte'?: number
   /**
-   * possible values are: [0, 1, 2, 3, 4, 5], can be a comma (AND) or pipe (OR) separated query
+   * possible values are?: [0, 1, 2, 3, 4, 5], can be a comma (AND) or pipe (OR) separated query
    */
-  with_status: string
+  with_status?: string
   /**
-   * possible values are: [flatrate, free, ads, rent, buy] use in conjunction with watch_region, can be a comma (AND) or pipe (OR) separated query
+   * possible values are?: [flatrate, free, ads, rent, buy] use in conjunction with watch_region, can be a comma (AND) or pipe (OR) separated query
    */
-  with_watch_monetization_types: string
+  with_watch_monetization_types?: string
   /**
    * use in conjunction with watch_region, can be a comma (AND) or pipe (OR) separated query
    */
-  with_watch_providers: string
-  without_companies: string
-  without_genres: string
-  without_keywords: string
-  without_watch_providers: string
+  with_watch_providers?: string
+  without_companies?: string
+  without_genres?: string
+  without_keywords?: string
+  without_watch_providers?: string
   /**
-   * possible values are: [0, 1, 2, 3, 4, 5, 6], can be a comma (AND) or pipe (OR) separated query
+   * possible values are?: [0, 1, 2, 3, 4, 5, 6], can be a comma (AND) or pipe (OR) separated query
    */
-  with_type: string
+  with_type?: string
 }
 
 export type DiscoverTVResponse = ResultsResponse<DiscoverShow>
